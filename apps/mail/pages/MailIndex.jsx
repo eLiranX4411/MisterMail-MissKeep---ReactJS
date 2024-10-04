ccdb8be7ccea8bfbe931f20f0eb73e25840741c2
eLiranX4411 <solidityart@gmail.com>
2024-10-04 15:18:59 +0300

    Add loadMails & loadNotes to apps indexes

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -3,7 +3,42 @@ const { Link, useSearchParams } = ReactRouterDOM
 
 import { showErrorMsg, showSuccessMsg, showUserMsg } from '../../../services/event-bus.service.js'
 import { mailService } from '../../../apps/mail/services/mail.service.js'
+import { getTruthyValues } from '../../../services/util.service.js'
 
 export function MailIndex() {
+  const [mail, setMails] = useState(null)
+  const [searchPrms, setSearchPrms] = useSearchParams()
+  const [filterBy, setFilterBy] = useState(mailService.getFilterFromSearchParams(searchPrms))
+
+  useEffect(() => {
+    loadMails()
+    setSearchPrms(getTruthyValues(filterBy))
+  }, [filterBy])
+
+  function loadMails() {
+    mailService
+      .query(filterBy)
+      .then(setMails)
+      .catch((err) => {
+        console.log('Problems getting mail:', err)
+      })
+  }
+
+  function onRemoveMail(mailId) {
+    mailService
+      .remove(mailId)
+      .then(() => {
+        setMails((mails) => mails.filter((mail) => mail.id !== mailId))
+        showSuccessMsg(`Note removed successfully!`)
+      })
+      .catch((err) => {
+        console.log('Problems removing mail:', err)
+        showErrorMsg(`Problems removing mail (${mailId})`)
+      })
+  }
+
+  function onSetFilterBy(filterBy) {
+    setFilterBy((preFilter) => ({ ...preFilter, ...filterBy }))
+  }
   return <div>mail app</div>
 }
--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -3,7 +3,42 @@ const { Link, useSearchParams } = ReactRouterDOM
 
 import { showErrorMsg, showSuccessMsg, showUserMsg } from '../../../services/event-bus.service.js'
 import { noteService } from '../../../apps/note/services/note.service.js'
+import { getTruthyValues } from '../../../services/util.service.js'
 
-export function NoteIndex() {
-  return <div>note app</div>
+export function MailIndex() {
+  const [note, setNotes] = useState(null)
+  const [searchPrms, setSearchPrms] = useSearchParams()
+  const [filterBy, setFilterBy] = useState(noteService.getFilterFromSearchParams(searchPrms))
+
+  useEffect(() => {
+    loadNotes()
+    setSearchPrms(getTruthyValues(filterBy))
+  }, [filterBy])
+
+  function loadNotes() {
+    noteService
+      .query(filterBy)
+      .then(setNotes)
+      .catch((err) => {
+        console.log('Problems getting note:', err)
+      })
+  }
+
+  function onRemoveNote(noteId) {
+    noteService
+      .remove(noteId)
+      .then(() => {
+        setNotes((notes) => notes.filter((note) => note.id !== noteId))
+        showSuccessMsg(`Note removed successfully!`)
+      })
+      .catch((err) => {
+        console.log('Problems removing note:', err)
+        showErrorMsg(`Problems removing note (${noteId})`)
+      })
+  }
+
+  function onSetFilterBy(filterBy) {
+    setFilterBy((preFilter) => ({ ...preFilter, ...filterBy }))
+  }
+  return <div>notes app</div>
 }
