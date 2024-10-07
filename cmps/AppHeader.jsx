26bb727c553360dfd10c91436d89eaa66f7781d8
eLiranX4411 <solidityart@gmail.com>
2024-10-07 15:57:28 +0300

    Add new logo and Svg cmp to notes

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -5,7 +5,6 @@ export function AppHeader() {
     <header className='app-header'>
       <Link to='/'>
         <img className='appsy-logo' src='../assets/img/Appsy.png' alt='' />
-        <h5 className='appsy'>Appsy</h5>
       </Link>
       <nav>
         <NavLink to='/'>Home</NavLink>
