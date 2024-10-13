40196cff097c6b6479559ede646c8890aef72772
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-10-13 12:35:57 +0300

    Created <MailDetails> and <MailCompose> components for future implementation. Updated route paths to allow access to these components.

---
+++ apps/mail/cmps/MailCompose.jsx
@@ -0,0 +1,7 @@
+export function MailCompose() {
+  return (
+    <section className='about'>
+      <h1>MailCompose</h1>
+    </section>
+  )
+}
---
+++ apps/mail/cmps/MailDetails.jsx
@@ -0,0 +1,7 @@
+export function MailDetails() {
+  return (
+    <section className='about'>
+      <h1>MailDetails</h1>
+    </section>
+  )
+}
