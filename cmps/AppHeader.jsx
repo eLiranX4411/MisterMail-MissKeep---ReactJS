7211600fe6cfe8956ac80e0e05f78bb05144b23b
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-10-14 12:19:18 +0300

    Added the files for the Books application and created corresponding links in the website's navigation.

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -26,6 +26,7 @@ export function AppHeader() {
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/mail'>Mail</NavLink>
         <NavLink to='/note'>Note</NavLink>
+        <NavLink to='/books'>Books</NavLink>
       </nav>
 
       {/* Hamburger Menu Icon */}
@@ -48,6 +49,9 @@ export function AppHeader() {
         </NavLink>
         <NavLink to='/note' onClick={toggleMenu}>
           Note
+        </NavLink>
+        <NavLink to='/books' onClick={toggleMenu}>
+          Books
         </NavLink>
       </nav>
     </header>
@@ -64,6 +68,7 @@ export function AppHeader() {
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/mail'>Mail</NavLink>
         <NavLink to='/note'>Note</NavLink>
+        <NavLink to='/books'>Books</NavLink>
       </nav>
 
       {/* Hamburger Menu Icon */}
@@ -86,6 +91,53 @@ export function AppHeader() {
         </NavLink>
         <NavLink to='/note' onClick={toggleMenu}>
           Note
+        </NavLink>
+        <NavLink to='/books' onClick={toggleMenu}>
+          Books
+        </NavLink>
+      </nav>
+    </header>
+  )
+
+  const BooksHeader = () => (
+    <header className='mail-app-header'>
+      {' '}
+      {/* מעדכן את הקלאס ל- mail-app-header כמו ב-Mail */}
+      <div className='mail-header-left'>
+        {' '}
+        {/* גם כאן אותו קלאס כמו ב-Mail */}
+        <img className='mail-logo' src='../../../apps/book/img/missBook.png' alt='Miss Books Logo' /> {/* קלאס mail-logo */}
+        <h2>Miss Books</h2>
+      </div>
+      <nav className='nav-desktop'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/mail'>Mail</NavLink>
+        <NavLink to='/note'>Note</NavLink>
+        <NavLink to='/books'>Books</NavLink>
+      </nav>
+      {/* Hamburger Menu Icon */}
+      <div className='hamburger-menu' onClick={toggleMenu}>
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+      </div>
+      {/* Mobile Navigation Menu */}
+      <nav className={`nav-mobile ${isMenuOpen ? 'show' : ''}`}>
+        <NavLink to='/' onClick={toggleMenu}>
+          Home
+        </NavLink>
+        <NavLink to='/about' onClick={toggleMenu}>
+          About
+        </NavLink>
+        <NavLink to='/mail' onClick={toggleMenu}>
+          Mail
+        </NavLink>
+        <NavLink to='/note' onClick={toggleMenu}>
+          Note
+        </NavLink>
+        <NavLink to='/books' onClick={toggleMenu}>
+          Books
         </NavLink>
       </nav>
     </header>
@@ -100,6 +152,7 @@ export function AppHeader() {
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/mail'>Mail</NavLink>
         <NavLink to='/note'>Note</NavLink>
+        <NavLink to='/books'>Books</NavLink>
       </nav>
 
       {/* Hamburger Menu Icon */}
@@ -123,15 +176,20 @@ export function AppHeader() {
         <NavLink to='/note' onClick={toggleMenu}>
           Note
         </NavLink>
+        <NavLink to='/books' onClick={toggleMenu}>
+          Books
+        </NavLink>
       </nav>
     </header>
   )
 
-  // Conditionally render the Google Keep-style header if at /note, else render the default header
+  // Conditionally render the appropriate header based on the current route
   if (location.pathname === '/note') {
     return <NoteHeader />
   } else if (location.pathname === '/mail') {
     return <MailHeader />
+  } else if (location.pathname === '/books') {
+    return <BooksHeader />
   } else {
     return <DefaultHeader />
   }
